f2f1508a1ffca69780dff44af1f9b850ae11f27e
Severina-anastasiia <severina.anastasiia@nure.ua>
2020-09-21 16:59:50 +0300

    Add new tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    ChangePosionOfNuber task1 = new ChangePosionOfNuber();
-	    task1.ChangePosionOfNumber();
-	    EndOfLessons task2 = new EndOfLessons();
-	    task2.EndOfLessons();
-	    TimeDifference task3 = new TimeDifference();
-	    task3.TimeDifference();
-	    Rally task4 = new Rally();
-	    task4.Rally();
-	    Snail task5 = new Snail();
-	    task5.Snail();
-	    Max task6 = new Max();
-	    task6.Max();
-	    Rook task7 = new Rook();
-	    task7.Rook();
-	    NumberOfEqualsOfThree task8 = new NumberOfEqualsOfThree();
-	    task8.Equals();
-        LargerThanThePrevious task9 = new LargerThanThePrevious();
-        task9.Larger();
+//	    ChangePosionOfNuber task1 = new ChangePosionOfNuber();
+//	    task1.ChangePosionOfNumber();
+//	    EndOfLessons task2 = new EndOfLessons();
+//	    task2.EndOfLessons();
+//	    TimeDifference task3 = new TimeDifference();
+//	    task3.TimeDifference();
+//	    Rally task4 = new Rally();
+//	    task4.Rally();
+//	    Snail task5 = new Snail();
+//	    task5.Snail();
+//	    Max task6 = new Max();
+//	    task6.Max();
+//	    Rook task7 = new Rook();
+//	    task7.Rook();
+//	    NumberOfEqualsOfThree task8 = new NumberOfEqualsOfThree();
+//	    task8.Equals();
+//        LargerThanThePrevious task9 = new LargerThanThePrevious();
+//        task9.Larger();
+        LargeNeighbors task10 = new LargeNeighbors();
+        task10.Large();
+        Rearrange task11 = new Rearrange();
+        task11.rearrange();
     }
 }
